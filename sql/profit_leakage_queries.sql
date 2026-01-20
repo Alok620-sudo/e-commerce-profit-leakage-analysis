21e92f9fb11e705e8d0347af1a92916d7941b6cf
Alok <rishu102resh@gmail.com>
2026-01-21 04:18:25 +0530

    Add sub-category profit analysis query

--- sql/profit_leakage_queries.sql
+++ sql/profit_leakage_queries.sql
@@ -6,3 +6,14 @@ SELECT
 FROM superstore
 GROUP BY category
 ORDER BY total_profit;
+
+
+-- Sub-category level profit analysis for Furniture
+SELECT
+    sub_category,
+    SUM(sales) AS total_sales,
+    SUM(profit) AS total_profit
+FROM superstore
+WHERE category = 'Furniture'
+GROUP BY sub_category
+ORDER BY total_profit;
